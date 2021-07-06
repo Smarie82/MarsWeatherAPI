1a19cd6eff94d3b316746cbd718c3e13daae91b3
Stephanie Falls <stephms@gmail.com>
2021-07-06 18:27:48 -0400

    commit something

--- Mars.js
+++ Mars.js
@@ -1,2 +1,19 @@
-const baseUrl = 'https: //api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0';
-const key = '0C11ZfycTILeBrJ6lrdb3jEy5WAN7hWysisFtKOa';
+const baseURl = "https://api.nasa.gov/insight_weather/?api_key=0C11ZfycTILeBrJ6lrdb3jEy5WAN7hWysisFtKOa&feedtype=json&ver=1.0";
+//const key = '0C11ZfycTILeBrJ6lrdb3jEy5WAN7hWysisFtKOa';
+
+const cardTitleOne = document.querySelector('.card-title-one');
+
+fetch(baseURl)
+    .then(response => response.json())
+    .then(json => displayWeather(json));
+
+function displayWeather(marsWeather) {
+        console.log(marsWeather);
+        
+    let title = document.createElement('h1');
+    title.className = 'card-title';
+    title.innetText = marsWeather.validity_check;
+
+    cardTitleOne.appendChild(title);
+
+    }
